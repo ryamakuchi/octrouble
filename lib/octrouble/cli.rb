56dff978c6a45d5d439865008635691a45a104a8
rry <r.yamakuchi@gmail.com>
2019-10-27 00:00:33 +0900

    リポジトリが見つからなかったときの処理を追加

--- lib/octrouble/cli.rb
+++ lib/octrouble/cli.rb
@@ -4,15 +4,32 @@ require 'octokit'
 
 module Octrouble
   class Cli < Thor
-    desc 'issues', 'get Issues data from GitHub API'
+    desc 'issues user/repo --page 1', 'get Issues data from GitHub API'
     option :page
+
     def issues(repo='rails/rails')
       page = options[:page] ? options[:page] : 1
-      issues = Octokit.list_issues(repo, page: page)
+      issues = get_issues(repo, page)
+
+      return if issues.nil?
+      return puts "Issue is not found." if issues == []
+
       issues.each do | issue |
         puts "\"#{issue.title[0, 30]}\"," \
           "\"#{issue.body[0, 50]}\"," \
           "\"#{issue.html_url}\"\n"
+      end
+    end
+
+    private
+
+    def get_issues(repo, page)
+      begin
+        Octokit.list_issues(repo, page: page)
+      rescue => e
+        puts "Repository \'#{repo}\' is not found."
+        puts "#{e.class}\n#{e.message}"
+        nil
       end
     end
   end
