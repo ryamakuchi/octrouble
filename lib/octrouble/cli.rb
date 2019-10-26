e39fa612a78438815c20448e57beef11d5a36c0e
rry <r.yamakuchi@gmail.com>
2019-10-26 12:54:05 +0900

    octokit を使って Issue を取得する処理を実装

--- lib/octrouble/cli.rb
+++ lib/octrouble/cli.rb
@@ -4,9 +4,16 @@ require 'octokit'
 
 module Octrouble
   class Cli < Thor
-    desc 'issues REPO_URL', 'get Issues data from GitHub API'
-    def issues(repo_url)
-      puts "issues REPO_URL: #{repo_url}"
+    desc 'issues', 'get Issues data from GitHub API'
+    option :page
+    def issues(repo='rails/rails')
+      page = options[:page] ? options[:page] : 1
+      issues = Octokit.list_issues(repo, page: page)
+      issues.each do | issue |
+        puts "\"#{issue.title[0, 30]}\"," \
+          "\"#{issue.body[0, 50]}\"," \
+          "\"#{issue.html_url}\"\n"
+      end
     end
   end
 end
