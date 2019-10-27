86160ac1361899353cbab4e3a02a21561ffe8ef9
rry <r.yamakuchi@gmail.com>
2019-10-27 17:02:38 +0900

    context 毎のテストを実装

--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -34,22 +34,61 @@ RSpec.describe Octrouble::Cli do
         '"title 1","body 1","html_url 1"\n"title 2","body 2","html_url 2"\n"title 3","body 3","html_url 3"'
       }.to_stdout
 
-      expect(octrouble).to have_received(:get_issues) do | repo, page |
-        expect(repo).to eq('rails/rails')
-        expect(page).to eq(1)
+      expect(octrouble).to have_received(:get_issues).with('rails/rails', 1)
+    end
+
+    context 'user/repo リポジトリが入力されている場合' do
+      it 'user/repo リポジトリの Issues が出力される' do
+        expect { octrouble.issues('user/repo') }.to output {
+          '"title 1","body 1","html_url 1"\n"title 2","body 2","html_url 2"\n"title 3","body 3","html_url 3"'
+        }.to_stdout
+
+        expect(octrouble).to have_received(:get_issues).with('user/repo', 1)
       end
     end
 
     context '--page 引数が入力されている場合' do
-      it 'そのページの Issues が出力される'
+      before do
+        allow(octrouble).to receive(:options) { { page: 2 } }
+      end
+
+      it 'そのページの Issues が出力される' do
+        expect { octrouble.issues }.to output {
+          '"title 1","body 1","html_url 1"\n"title 2","body 2","html_url 2"\n"title 3","body 3","html_url 3"'
+        }.to_stdout
+
+        expect(octrouble).to have_received(:get_issues).with('rails/rails', 2)
+      end
     end
 
     context 'Issues が 0件だった場合' do
-      it 'Issue is not found. という結果が出力される'
+      before do
+        allow(octrouble).to receive(:get_issues).and_return([])
+      end
+
+      it 'Issue is not found. という結果が出力される' do
+        expect { octrouble.issues }.to output { 'Issue is not found.' }.to_stdout
+        expect(octrouble).to have_received(:get_issues)
+      end
     end
 
     context '該当するリポジトリが存在しない場合' do
-      it 'Repository is not found. という結果が出力される'
+      before do
+        allow(octrouble).to receive(:get_issues).and_raise(
+          Octokit::NotFound,
+          "Octokit::NotFound\n" \
+          "GET https://api.github.com/repos/ryamakuchi/none/issues?page=1: 404 - Not Found // See: https://developer.github.com/v3/issues/#list-issues-for-a-repository"
+        ).and_return(nil)
+      end
+
+      it 'Repository is not found. という結果が出力される' do
+        expect { octrouble.issues('ryamakuchi/none') }.to output {
+          "Octokit::NotFound\n" \
+          "GET https://api.github.com/repos/ryamakuchi/none/issues?page=1: 404 - Not Found // See: https://developer.github.com/v3/issues/#list-issues-for-a-repository\n" \
+          "Repository 'ryamakuchi/none' is not found."
+        }.to_stdout
+        expect(octrouble).to have_received(:get_issues).with('ryamakuchi/none', 1)
+      end
     end
   end
 end
